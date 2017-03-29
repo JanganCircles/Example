4a3b35ae4adb063abd7bc777ef8306c13319f76a
Sungwoo97 <gwaja97@gmail.com>
2017-03-29 21:50:59 +0900

    메모(수정중)

--- Exsistence/Assets/Script/scMemo.cs
+++ Exsistence/Assets/Script/scMemo.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class scMemo : MonoBehaviour {
     private string file;
     private string textValue;
+    public bool memoState = false;
+    public MeshRenderer memoNote;
     // Use this for initialization
     void Start()
     {
-
-         file = @"C:\Users\admin\Documents\GitHub\Exsistence-master(1)\Exsistence-master";
+        memoNote = gameObject.GetComponent<MeshRenderer>();
+         file = @"C:\Users\admin\Documents\GitHub\Exsistence";
          textValue = System.IO.File.ReadAllText(file);
     }
 	
 	// Update is called once per frame
 	void Update () {
         print(textValue);
-	}
+        if (memoState != false)
+        {
+            memoNote.enabled = false;
+        }
+        // memoCheck();
+    }
+    void memoCheck()
+    {
+        if(memoState != false)
+        {
+            memoNote.enabled = false;
+        }
+    }
 }
