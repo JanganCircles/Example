cd974b083813af7dc8ec01becea9ddb695b057b1
dusdnr33 <qewrytu@naver.com>
2017-03-30 04:58:43 +0900

    성우
    상호작용과 문열기, 빌보드 스크립트 수정

---
+++ Exsistence/Assets/Script/scDoor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scDoor : MonoBehaviour {
+    private bool dOn = false;
+    private bool dOff;
+    private Transform doorTr;
+	// Use this for initialization
+	void Start () {
+        doorTr= GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dOn)
+        {
+            StartCoroutine(open_Door());
+            //transform.Rotate(new Vector3(0, 90, 0));
+           
+                dOn = false;
+        }
+       /* if (dOff)
+        {
+            transform.Rotate(new Vector3(0, -90, 0));
+            dOff = false;
+        }*/
+        
+	}
+    public void Close()
+    {
+        dOff = true;
+    }
+    public void Open()
+    {
+        dOn = true;
+    }
+    IEnumerator open_Door()
+    {
+        //
+        // {
+        while (true)
+        {
+            if (doorTr.transform.rotation.y >= 90)
+            {
+
+                transform.Rotate(new Vector3(0, 10, 0));
+
+                yield return null;
+            }
+        }
+           
+       // }
+    }
+    }
+
---
+++ Exsistence/Assets/Script/scHud.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scHud : MonoBehaviour {
+    private Text hudText;                // 텍스트
+    private float objside;                  // 거리 계산
+   // public bool interaction = false;        // 상호작용 on/off
+    public Transform interactionTr;        // 상호작용 받는 것의 위치 값 받아오는 변수
+    private Transform playerTr;             // 플레이어의 위치 값을 받아오는 변수
+    public MeshRenderer interactionMesh;   // 상호작용이 되면 Meshrenderer on
+    public CapsuleCollider playerRd;       // collider radius 값과 거리를 같게 해줄 변수
+    public GameObject door;
+    private bool doorOn= false;
+    // Use this for initialization
+    void Start()
+    {
+        interactionTr.GetComponent<Transform>();
+        playerTr.GetComponent<Transform>();
+        interactionMesh.GetComponent<MeshRenderer>().enabled = true;
+       
+        playerRd.GetComponent<CapsuleCollider>();
+        objside = playerRd.radius;
+
+        // hudText.GetComponent<Text>();
+       // interactionMesh.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && interactionMesh.enabled != false)
+        {
+            // interactionMesh.enabled = true;
+            print("e눌림");
+            if (doorOn == false)
+            {
+                door.SendMessage("Open");
+            }
+            else if(doorOn == true)
+            {
+                door.SendMessage("Close");
+            }
+        }
+
+        /* float distance = Vector3.Distance(interactionTr.position, transform.position);
+          if (distance + objside <= objside)
+          */
+    }
+        void OnTriggerEnter(Collider oncol)
+    {
+        
+       if (oncol.tag == "Player")
+            {
+                Debug.Log("?");
+                                     hud_on();
+            
+        }
+       /* if (interactionMesh != false && oncol.gameObject.tag == "interaction"&& Input.GetKeyDown(KeyCode.E))
+        {         
+            print("e눌림");
+                    }*/
+    }
+    void OnTriggerExit(Collider offcol)
+    {
+        Debug.Log("2");
+        hud_off();
+    }
+    public void hud_on()
+    {
+        interactionMesh.enabled = true;
+      
+    }
+    public void hud_off()
+    {
+        
+     interactionMesh.enabled = false;
+    }
+}
