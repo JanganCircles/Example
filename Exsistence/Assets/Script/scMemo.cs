936841657c19533f4a6f4d9764dfc74f3fb454e6
Sungwoo97 <gwaja97@gmail.com>
2017-03-29 21:22:43 +0900

    메모

---
+++ Exsistence/Assets/Script/scMemo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scMemo : MonoBehaviour {
+    private string file;
+    private string textValue;
+    // Use this for initialization
+    void Start()
+    {
+
+         file = @"C:\Users\admin\Documents\GitHub\Exsistence-master(1)\Exsistence-master";
+         textValue = System.IO.File.ReadAllText(file);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        print(textValue);
+	}
+}
