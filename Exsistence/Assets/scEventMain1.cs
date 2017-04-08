3d71ea1946ea57e84fd2ef03efaeebb6c977df5e
Nichi72 <money970709@naver.com>
2017-04-08 12:06:45 +0900

    메인 이벤트 만드는중
    감은 잡음

---
+++ Exsistence/Assets/scEventMain1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scEventMain1 : MonoBehaviour {
+    public GameObject gameobj;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+
+    }
+
+    void EVT()
+    {
+        gameobj.SetActive(false);
+    }
+
+
+
+}
