68ab53e8723d1259edbb137ef4ecfd62c18bdc4f
Nichi72 <money970709@naver.com>
2017-04-12 05:22:34 +0900

    메인 이벤트 1 만드는중
    동빈이 한테 받은 맥스 파일이 FBX가 아니라서
    우리집에서 임포트가 안됨
    
    Fade in도 구현 할려고했는데
    전체가 까매지는 것이 아니라
    실제로 눈동자가 꿈벅이는 것 처럼 Fade in을 만들고 싶어서 fade in은 보류중

--- Exsistence/Assets/scEventMain1.cs
+++ Exsistence/Assets/scEventMain1.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class scEventMain1 : MonoBehaviour {
     public GameObject gameobj;
+    public SpriteRenderer blackFade;
 	// Use this for initialization
 	void Start () {
         
@@ -19,7 +20,19 @@ public class scEventMain1 : MonoBehaviour {
     {
         gameobj.SetActive(false);
     }
+    IEnumerator FadeIn()
+    {
+        for (float i = 1f; i >= 0;i-=0.1f)
+        {
+            Color color = new Vector4(1, 1, 1, i);
+            blackFade.color = color;
 
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+
+    
 
 
 }
