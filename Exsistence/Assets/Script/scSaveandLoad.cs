eae7a544a9b86ce0183a31a351bd061802287116
dusdnr33 <qewrytu@naver.com>
2017-04-13 05:05:16 +0900

    Save메뉴 및 저장 스크립트 추가 테스트 안해봄

--- Exsistence/Assets/Script/scSaveandLoad.cs
+++ Exsistence/Assets/Script/scSaveandLoad.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using System.IO;
 
 public class scSaveandLoad : MonoBehaviour
-{
-	void Update ()
+{   
+    public void Save()
     {
-		
-	}
+        string fileName = "Save File " + this.gameObject.name.Substring(9);
+
+        StreamWriter stream = new StreamWriter(fileName);
+
+        if (GameObject.Find("Player"))
+        {
+            Transform TPlayer = GameObject.Find("Player").transform;
+            stream.WriteLine(Application.loadedLevelName);
+            stream.WriteLine(TPlayer);
+            stream.WriteLine(scGameManager.instance.eventIndex);
+
+            Debug.Log("Scene Name : " + Application.loadedLevelName + ", Player Transform" + TPlayer.position + ", Event Index" + scGameManager.instance.eventIndex);
+        }
+    }
 }
