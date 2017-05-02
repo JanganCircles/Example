591ca3fef419333cf12b4d61a6b0af7e85ec917a
Sungwoo97 <gwaja97@gmail.com>
2017-05-02 21:24:55 +0900

    1차 수정

--- Exsistence/Assets/Script/scObject.cs
+++ Exsistence/Assets/Script/scObject.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class scHud : MonoBehaviour {
+public class scObject : MonoBehaviour
+{
     private Text hudText;                // 텍스트
     private float objside;                  // 거리 계산
-   // public bool interaction = false;        // 상호작용 on/off
+                                            // public bool interaction = false;        // 상호작용 on/off
     public Transform interactionTr;        // 상호작용 받는 것의 위치 값 받아오는 변수
     private Transform playerTr;             // 플레이어의 위치 값을 받아오는 변수
     public MeshRenderer interactionMesh;   // 상호작용이 되면 Meshrenderer on
     public CapsuleCollider playerRd;       // collider radius 값과 거리를 같게 해줄 변수
-    public scDoor door;
-    public bool doorOn=true;
-   // public scInterObject interObject;
-   // public bool objectOn=true;
+   // public scDoor door;
+  //  public bool doorOn = false;
+     public scInterObject interObject;
+     public bool objectOn=true;
     // Use this for initialization
     void Start()
     {
         interactionTr.GetComponent<Transform>();
         playerTr.GetComponent<Transform>();
         interactionMesh.GetComponent<MeshRenderer>().enabled = true;
-       
+
         playerRd.GetComponent<CapsuleCollider>();
         objside = playerRd.radius;
 
         // hudText.GetComponent<Text>();
-       // interactionMesh.enabled = true;
+        // interactionMesh.enabled = true;
     }
 
     // Update is called once per frame
@@ -37,13 +38,13 @@ public class scHud : MonoBehaviour {
         {
             // interactionMesh.enabled = true;
             print("e눌림");
-            
-            if (doorOn == true)
+
+           /* if (doorOn == false)
             {
 
-                door.SendMessage("Open");                
-            }
-            /*if(objectOn == true)
+                door.SendMessage("Open");
+            }*/
+            if(objectOn == true)
             {
                 interObject.SendMessage("Play");
             }
@@ -58,25 +59,25 @@ public class scHud : MonoBehaviour {
           if (distance + objside <= objside)
           */
     }
-        void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider col)
     {
 
         if (col.gameObject.tag == "Player")
         {
-            Debug.Log("?");            
+            Debug.Log("?");
             interactionMesh.enabled = true;
             //door = col.GetComponent<scDoor>();
         }
-       /* if (interactionMesh != false && oncol.gameObject.tag == "interaction"&& Input.GetKeyDown(KeyCode.E))
-        {         
-            print("e눌림");
-                    }*/
+        /* if (interactionMesh != false && oncol.gameObject.tag == "interaction"&& Input.GetKeyDown(KeyCode.E))
+         {         
+             print("e눌림");
+                     }*/
     }
     void OnTriggerExit(Collider col)
     {
         Debug.Log("2");
         interactionMesh.enabled = false;
-      
+
     }
-  
+
 }
