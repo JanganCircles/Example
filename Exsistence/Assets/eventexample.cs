3875f9764217d3f31e9fcd7ae7c9685909b2bbaf
SolarCcc <wooshwoo@naver.com>
2017-05-11 00:54:27 +0900

    신현
    이벤트 연습

--- Exsistence/Assets/eventexample.cs
+++ Exsistence/Assets/eventexample.cs
@@ -8,6 +8,7 @@ public class eventexample : MonoBehaviour ,iEvent
     public void GetiEvent(object obj) { (obj as scEventRunner).SetUpEvt(this); }
     Transform Tr;
     public Vector3 Pos;
+
     public void Start()
     {
         Tr = transform;
