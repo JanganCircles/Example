1a06d8c28b3cba7940809ff4734e80850fe02bb4
daldu <gngpgly3@naver.com>
2017-05-12 00:04:50 +0900

    아영 작업중

--- Exsistence/Assets/Script/scMsg.cs
+++ Exsistence/Assets/Script/scMsg.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scMsg : MonoBehaviour {
     private static List<GameObject> msgList = new List<GameObject>();
+    public static GameObject msgText;
 	// Use this for initialization
 	void Start () {
-        msgList = null;
+        //msgList = null;
+        msgText = transform.GetChild(0).gameObject;
+        CreateNewMessage("안녕");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    public static void CreateNewMessage(string str) {
-
+    public static void CreateNewMessage(string str) { 
+        msgList.Add(Instantiate(msgText));
+        msgList[0].GetComponent<Transform>().GetComponent<Text>().text = str;
     }
+    public static void DeleteMessage(int index) {
+        msgList.RemoveAt(index);
+    }
+    public static void DeleteMessage(string TextValue) {
+        msgList.Remove(msgList.Find(msgList => msgList.name.Contains("TextValue")));
+    }
+
 
 }
