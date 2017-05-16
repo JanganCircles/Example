a9e9ec48bd1529fe2990555ef227ed9f09d85360
SolarCcc <wooshwoo@naver.com>
2017-05-17 00:08:17 +0900

    신현
    살펴보기가 없어서 다시 싱크하려고 커밋함

---
+++ Exsistence/Assets/Script/Event/scEvt5th.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scEvt5th : MonoBehaviour, iEvent
+{
+    public void GetiEvent(object obj) { (obj as scEventRunner).SetUpEvt(this); }
+
+    void iEvent.Run()
+    {
+        //gameObject.SetActive(true);
+        gameObject.transform.GetChild(2).gameObject.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
