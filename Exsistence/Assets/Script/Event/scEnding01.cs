d527d037f8512565d42092601decd38d15ee706c
gurtn08 <gurtn08@naver.com>
2017-05-23 20:19:48 +0900

    엔딩 01 여전히 진행중..

--- Exsistence/Assets/Script/Event/scEnding01.cs
+++ Exsistence/Assets/Script/Event/scEnding01.cs
@@ -11,6 +11,10 @@ public class scEnding01 : MonoBehaviour
     private AudioSource audio;
     private int count = 0;
 
+    void Start()
+    {
+        audio = transform.GetComponent<AudioSource>();
+    }
 	void Update ()
     {
         if(scGameManager.instance.eventIndex == -1)
@@ -31,11 +35,15 @@ public class scEnding01 : MonoBehaviour
 
     void Ending()
     {
+        GameObject Player = Object[0].transform.parent.gameObject;
+
         float distance = 0;
+        float i = 0;
+
         distance = Vector3.Distance(Object[0].transform.position, Object[1].transform.position);
 
         if (distance >= 0.5)
-            transform.position = Vector3.forward;
+           Player.transform.position = new Vector3(Player.transform.position.x + i++,0,Player.transform.position.z + i++);
     }
 
     void OpenDoor()
