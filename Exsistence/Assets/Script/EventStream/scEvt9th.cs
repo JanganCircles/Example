c7a9d44d537109e7757fb7e85d8cfe92b17994e0
Nichi72 <money970709@naver.com>
2017-05-24 22:41:11 +0900

    main씬에서 작업하세요
    큿

--- Exsistence/Assets/Script/EventStream/scEvt9th.cs
+++ Exsistence/Assets/Script/EventStream/scEvt9th.cs
@@ -9,8 +9,7 @@ public class scEvt9th : MonoBehaviour, iEvent
     public GameObject Bag;
 
 
-    public GameObject Posi; // ? 
-    public GameObject Player;
+ 
 
 
     public void GetiEvent(object obj) { (obj as scEventRunner).SetUpEvt(this); }
@@ -21,25 +20,6 @@ public class scEvt9th : MonoBehaviour, iEvent
         ShockBox.SetActive(true);
         Bag.SetActive(true);
 
-
-
-
-        DontDestroyOnLoad(Player);
-        Posi.transform.position = Player.transform.position;
-        Posi.transform.rotation = Player.transform.rotation;
-        Debug.Log("Player.transform.position" + Player.transform.position);
-        Debug.Log("Player.transform.rotation" + Player.transform.rotation);
-        Debug.Log("Posi.transform.position" + Posi.transform.position);
-        Debug.Log("Posi.transform.rotation" + Posi.transform.rotation);
-        SceneManager.LoadScene("EvtShinHyeon");
-        //Player = gameObject.transform.Find("Player").gameObject;
-        Player.transform.position = Posi.transform.position;
-        Player.transform.rotation = Posi.transform.rotation;
-
-        Debug.Log("7272Player.transform.position" + Player.transform.position);
-        Debug.Log("7272Player.transform.rotation" + Player.transform.rotation);
-        Debug.Log("7272Posi.transform.position" + Posi.transform.position);
-        Debug.Log("7272Posi.transform.rotation" + Posi.transform.rotation);
 
     }
 
